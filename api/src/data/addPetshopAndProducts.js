9e1d574bbf5fb256742c3c5f1259e2e8badf1a4a
Samuel Gonçalves <samuelesilva@hotmail.com>
2025-01-27 00:13:29 -0300

    Criação de script e migration no MongoDB

---
+++ api/src/data/addPetshopAndProducts.js
@@ -0,0 +1,23 @@
+const Petshop = require('../models/petshop')
+const Product = require('../models/product')
+const petshops = require('./petfood.json')
+
+
+require('../database')
+
+const addPetshopAndProducts = async () => {
+    try {
+        for (let petshop of petshops) {
+            const newPetshop = await new Petshop(petshop).save()
+            await Product.insertMany(petshop.produtos.map(
+                (p) => ({ ...p, petshop_id: newPetshop._id}))
+            )
+        }
+
+        console.log('Final do Script')
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+addPetshopAndProducts()
