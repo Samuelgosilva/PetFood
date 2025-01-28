9aa94d62e93b0269b9028a15eeba85380903d5d7
Samuel Gonçalves <samuelesilva@hotmail.com>
2025-01-28 13:56:15 -0300

    Finalizando rota petshop

--- api/src/routes/main.routes.js
+++ api/src/routes/main.routes.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+const Petshop = require('../models/petshop')
+const Product = require('../models/product')
+
+router.get('/petshops', async (req, res) => {
 
     try {
-        res.json({ error: false, message: 'Hello World.' })
+        const petshops = await Petshop.find()
+        res.json({ error: false, petshops })
+    } catch (err) {
+        res.json({error: true, message: err.message})
+    }
+})
+
+router.get('/petshop/:id', async (req, res) => {
+
+    try {
+        const petshop = await Petshop.findById(req.params.id)
+        let products = await Product.find({
+            petshop_id: petshop._id
+        })
+
+        res.json({ error: false, petshop: { ...petshop._doc, products} })
     } catch (err) {
         res.json({error: true, message: err.message})
     }
